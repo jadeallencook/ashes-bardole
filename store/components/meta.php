1ea16f20934ff5408161a8f9415b9fbddaa97dd4
ashesbardole <ashesbardole@Ashleys-MacBook-Air.local>
2018-01-01 20:48:06 -0700

    Editing Store content and visuals

--- store/components/meta.php
+++ store/components/meta.php
@@ -7,5 +7,6 @@
 <meta name="robots" content="index, follow">
 <meta name="revisit-after" content="3 month">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="shortcut icon" type="image/x-icon" href="images/favicon.ico">
+<link rel="icon" type="image/png" href="../../images/favicon-32x32.png" sizes="32x32" />
+<link rel="icon" type="image/png" href="../../images/favicon-16x16.png" sizes="16x16" />
 <link rel="apple-touch-icon" href="apple-touch-icon.png">
--- store/components/sale-banner.php
+++ store/components/sale-banner.php
@@ -1,7 +1,7 @@
         <div class="slider__container slider--four">
                 <div class="slider__activation__wrap owl-carousel owl-theme">
                     <!-- Start Single Slide -->
-                    <div class="slide slider__fixed--height" style="background: rgba(0, 0, 0, 0) url(images/slider/bg/3.jpg) no-repeat scroll center center / cover ;">
+                    <div class="slide slider__fixed--height" style="background: rgba(0, 0, 0, 0) url(images/slider/bg/1.jpg) no-repeat scroll center center / cover ;">
                         <div class="container">
                             <div class="row">
                                 <div class="col-md-7 col-lg-6 col-lg-offset-6 col-md-offset-5 col-sm-10 col-sm-offset-2 col-xs-12">
